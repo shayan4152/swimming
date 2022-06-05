8fb8884cd6c5090f6a9cb8f47f2523d438d3f7ed
shayan4152 <100376933+shayan4152@users.noreply.github.com>
2022-06-05 19:38:46 +0500

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
+
+module.exports = nextConfig
+
+// next.config.js
+module.exports = {
+  images: {
+    domains: ['th.bing.com'],
+  },
+}
